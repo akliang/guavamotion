0804cf3a6773e6278604a018c421da407f7fa742
Albert Liang <albert.liang@gmail.com>
2015-04-08 02:52:38 -0400

    turend off latex formating for plots, added a fourth subplot to show the histogram of the time between reads

--- plot_acc_sddata.m
+++ plot_acc_sddata.m
@@ -1,5 +1,7 @@
 
-fname='/Volumes/ACCELONE/ACC014.TXT';
+set(0,'DefaultTextInterpreter','none');
+
+fname='/Users/moonbeam/Desktop/g_test/2g_64ms_throw.TXT';
 [p f e]=fileparts(fname);
 
 q=load(fname);
@@ -11,15 +13,21 @@ c.x=4;
 c.y=5;
 c.z=6;
 
-fh=subplot(3,1,1)
+fh=subplot(4,1,1)
 plot(q(:,c.milli),q(:,c.x))
 title(sprintf('%s\nwalking straight line\nx accel',fname))
-subplot(3,1,2)
+subplot(4,1,2)
 plot(q(:,c.milli),q(:,c.y))
 title('y accel')
-subplot(3,1,3)
+subplot(4,1,3)
 plot(q(:,c.milli),q(:,c.z))
 title('z accel');
 xlabel('time (ms)')
+subplot(4,1,4)
+hist(diff(q(:,c.milli)),200)
+title('histogram of time between reads');
+xlabel('time between reads (ms)');
+ylabel('count');
+
 
 print([f '.png'],'-dpng');
